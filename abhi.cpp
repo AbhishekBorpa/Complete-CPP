699d12e9b9deb9432a9fade59c7f1442e4abe7fd
Abhishek <abhishekborpa@gmail.com>
2024-09-13 21:56:10 +0530

    aa

--- abhi.cpp
+++ abhi.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 using namespace std;
+int abhi(int a,int b){
+    int c = a+b;
+    return c;
+}
 int main()
 {
     std::cout<<"hey,what  are you doing"<<std::endl;
@@ -8,5 +12,17 @@ int main()
     /*
     multiline comment
     */
+//    errors and warnings
+/*
+compile-time  error : syntax error,
+runtime error :  logical error
+warnings : can be problem
+
+
+*/
+//  statement and  functions
+
+
+
     return 0;
 }
