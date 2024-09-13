2b264860247cb759c811aac9ccd92ceccc44e0e3
Abhishek <abhishekborpa@gmail.com>
2024-09-13 22:25:41 +0530

    new

--- abhi.cpp
+++ abhi.cpp
@@ -4,11 +4,21 @@ int abhi(int a,int b){
     int c = a+b;
     return c;
 }
+
+int mul(int c,int d){
+    int m =  c*d;
+    return m;
+}
 int main()
 {
     std::cout<<"hey,what  are you doing"<<std::endl;
     std::cout << "Hello World" << std::endl;
     // single line code comment
+    int first_num =10;
+    int second_num = 20;
+    // int sum = first_num+second_num;
+    cout<<mul(10,20)<<endl;
+    cout<<abhi(10,20)<<endl;
     /*
     multiline comment
     */
@@ -22,7 +32,12 @@ warnings : can be problem
 */
 //  statement and  functions
 
-
-
+// std::cout : output <<
+// std:cin : input >>
+// std:cerr : print error message <<
+// srd:clog : log message <<
+string name;
+cout<<"Enter Your Name:";
+cin>> name;
     return 0;
 }
