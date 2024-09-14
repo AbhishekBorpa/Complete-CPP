8a98db52113ee409fc139951afd00270f380f23d
Abhishek <abhishekborpa@gmail.com>
2024-09-14 12:34:23 +0530

    ff

--- 03_cpp/cpp_opps.cpp
+++ 03_cpp/cpp_opps.cpp
@@ -2,19 +2,27 @@
 using namespace std;
 
 int main(){
+    // Basic ops; 
+
+
     int a = 10;
     int b = 20;
-    int c = a-b;
-    int d = a+b;
-    int e = a*b;
-    int f = b/a;
+    int c = 14;
+    int d = 15;
+    int e = 16;
+    int f = 17;
     int g = b%3;
 
 
-    cout<<c<<endl;
-    cout<<d<<endl;
-    cout<<e<<endl;
-    cout<<f<<endl;
-    cout<<g<<endl;
+    // cout<<c<<endl;
+    // cout<<d<<endl;
+    // cout<<e<<endl;
+    // cout<<f<<endl;
+    // cout<<g<<endl;
+
+    // precendence(which ops to do first) and associativity(dir or order)
+
+    int  result = (a+(b*(c/d))-(e*f));
+    cout<<result<<endl;
     return 0;
 }
