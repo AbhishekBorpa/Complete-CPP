1fa61e85bcea1945055a0ab57536b4d2a5e9c1c4
Abhishek <abhishekborpa@gmail.com>
2024-09-14 13:27:06 +0530

    ttt

--- 03_cpp/cpp_opps.cpp
+++ 03_cpp/cpp_opps.cpp
@@ -5,28 +5,35 @@ int main(){
     // Basic ops; 
 
 
-    int a = 10;
-    int b = 20;
-    int c = 14;
-    int d = 15;
-    int e = 16;
-    int f = 17;
-    int g = b%3;
+    // int a = 10;
+    // int b = 20;
+    // int c = 14;
+    // int d = 15;
+    // int e = 16;
+    // int f = 17;
+    // int g = b%3;
 
 
-    // cout<<c<<endl;
-    // cout<<d<<endl;
-    // cout<<e<<endl;
-    // cout<<f<<endl;
-    // cout<<g<<endl;
+    // // cout<<c<<endl;
+    // // cout<<d<<endl;
+    // // cout<<e<<endl;
+    // // cout<<f<<endl;
+    // // cout<<g<<endl;
 
-    // precendence(which ops to do first) and associativity(dir or order)
+    // // precendence(which ops to do first) and associativity(dir or order)
 
-    // int  result = (a+(b*(c/d))-(e*f));
-    // cout<<result<<endl;
+    // // int  result = (a+(b*(c/d))-(e*f));
+    // // cout<<result<<endl;
 
-    // prefix(increment and decrement) and postfix( incre and decre)
-    // f =f+1 ;
-    cout<<++f<<endl;
+    // // prefix(increment and decrement) and postfix( incre and decre)
+    // // f =f+1 ;
+    // cout<<++f<<endl;
+
+    //  assignment operator
+    int ram = 10;
+    ram+= 1;
+    ram *=3;
+    ram%= 6;
+    cout<<ram<<endl;
     return 0;
 }
