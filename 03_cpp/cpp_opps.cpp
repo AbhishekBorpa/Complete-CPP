66e5ff8e360f4f315cb13773dc6aff41ab051b01
Abhishek <abhishekborpa@gmail.com>
2024-09-14 12:19:33 +0530

    nn

---
+++ 03_cpp/cpp_opps.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int c = a-b;
+    int d = a+b;
+    int e = a*b;
+    int f = b/a;
+    int g = b%3;
+
+
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<f<<endl;
+    cout<<g<<endl;
+    return 0;
+}
