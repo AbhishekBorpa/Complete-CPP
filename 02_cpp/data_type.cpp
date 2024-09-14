154286aa849f681b62622cca17beb87aa55e77c1
Abhishek <abhishekborpa@gmail.com>
2024-09-14 11:11:05 +0530

    as

--- 02_cpp/data_type.cpp
+++ 02_cpp/data_type.cpp
@@ -4,6 +4,17 @@
 
 int main(){
     int a=10;
+    // signed int val1 = -5;
+    unsigned int val1 = 5;
+    // short,int,long,long long
+
+    long a =10;
+    long int b =10;
+    signed long int c =15;
+    unsigned long int d = 17;
+    long long int r = 18;
+    int vall2 = 5;
+    std::cout<<sizeof(val1)<<std::endl;
     std::cout<<a<<std::endl;
     return 0;
 }
